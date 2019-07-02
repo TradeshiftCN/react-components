33b024f3798ca74515e41b20d472743f5ad591f3
kanon <kanonwang@outlook.com>
2019-07-02 16:35:05 +0800

    auto-deployed by gh-pages

--- HashRouter.js
+++ HashRouter.js
@@ -1,7 +1,7 @@
-import createHashSource from 'hash-source'
-import { createHistory } from '@reach/router'
+import createHashSource from 'hash-source';
+import { createHistory } from '@reach/router';
 
-const history = createHistory(createHashSource())
+const history = createHistory(createHashSource());
 
 // history.listen(({ location }) => {
 //   // react router updates only iframe url hash, so we have to change window.top location manually
@@ -15,5 +15,5 @@ const history = createHistory(createHashSource())
 //
 // console.log('window.top.location.hash', window.top.location.hash)
 
-export const HashRouter = history
-export const navigate = history.navigate
+export const HashRouter = history;
+export const navigate = history.navigate;
